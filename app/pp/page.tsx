222a89bc9fe877d89c6dc07f3611c75780431879
arian <arian@MacBook-Pro.local>
2024-02-13 10:45:38 +0700

    Privacy and policy page

--- app/pp/page.tsx
+++ app/pp/page.tsx
@@ -1,9 +1,227 @@
 import { SectionWrapper } from '@/app/ui/wrapper';
 
 export default function PrivacyPolicyPage() {
+  const subTitleClass = 'text-[#287B49] font-semibold text-[14px] mb-2 mt-10 tracking-widest';
+  const desc = 'font-medium text-base text-black';
+
   return (
-    <SectionWrapper padding='high'>
-      <h1>PrivacyPolicyPage</h1>
+    <SectionWrapper className='py-5 pb-20 lg:pt-16 lg:pb-40' padding='high'>
+      <h1 className='text-[20px] lg:text-[32px] font-medium'>Privacy Policy</h1>
+      <p className='text-base text-black font-medium mt-3'>Effective Date 30 January 2024</p>
+
+      <p className={subTitleClass}>INTRODUCTIONS</p>
+      <p className={desc}>
+        PT Carbonx Bumi Harmoni (hereinafter referred to as “CarbonX”, “us”, “we”, or “our”) understands how critical it
+        is to safeguard your personal data. With the creation of this Privacy Policy, we commit to manage and safeguard
+        any personal information or data that Users may provide on the https://carbonxco.com/ website and its derivative
+        sites (collectively the “Site” or “Service”). This Privacy Policy describes the personal data CarbonX collects
+        about you from the website that you liked to; how we use and protect the data; and the choices you have
+        regarding your personal data. Your visit to the Site is subject to this Privacy Policy and to our Terms &
+        Conditions. 
+      </p>
+      <p className={`${desc} mt-3`}>
+        We use your data to operate and enhance our Site. By accessing our Site, you consent and agree to the
+        collection, processing, and use of information in line with this policy. The terminology used in this Privacy
+        Policy have the same meanings as those found in our Terms and Conditions, unless otherwise indicated. Our Terms
+        and Conditions (“Terms”) govern all use of our Site and together with the Privacy Policy constitutes your
+        agreement with us.
+      </p>
+
+      <p className={subTitleClass}>DEFINITIONS</p>
+      <p className={`${desc} mt-3`}>
+        <strong>“Personal Data”</strong> is any information that relates to an identified or identifiable living
+        individual.
+      </p>
+      <p className={`${desc} mt-3`}>
+        <strong>“User”</strong> or <strong>“Data Subject”</strong> is the identified or identifiable living individual
+        to whom personal data relates. In this Agreement, the Data Subject is also referred to as the User.
+      </p>
+      <p className={`${desc} mt-3`}>
+        <strong>“Usage Data”</strong> is any data about how a product or service is used.
+      </p>
+      <p className={`${desc} mt-3`}>
+        <strong>“Data Controller”</strong> determines the purposes for which and the means by which your personal data
+        is processed.
+      </p>
+      <p className={`${desc} mt-3`}>
+        <strong>“Data Processors”</strong> process personal data only on behalf of the controller. This refers to a
+        third party external to CarbonX.
+      </p>
+      <p className={`${desc} mt-3`}>
+        <strong>“Cookies”</strong> are text files with small pieces of data that are used to identify your computer as
+        you use a network.
+      </p>
+      <p className={`${desc} mt-3`}>
+        <strong>“Service”</strong> or <strong>“Site”</strong> means the website operated by CarbonX,
+        https://carbonxco.com/ and its derivative sites.
+      </p>
+
+      <p className={subTitleClass}>DATA COLLECTED AND PROCESSED BY CARBONX</p>
+      <p className={desc}>
+        While accessing our Site, we may ask you to provide us with certain personally identifiable information that can
+        be used to get in touch with you or identify you (“Personal Data”). This may include, but is not limited to:
+      </p>
+      <ol className='list-decimal pl-7 my-5'>
+        <li className={desc}>Identity Data including first and last name, date of birth</li>
+        <li className={desc}>Contact Data including address, email address and phone number</li>
+        <li className={desc}>Interaction Data</li>
+        <li className={desc}>Cookies and Usage Data</li>
+      </ol>
+      <p className={desc}>
+        We collect personal information you choose to provide, e.g., through registrations, applications, and surveys,
+        and in connection with your inquiries. We may use your Personal Data to contact you with promotional or
+        marketing materials and other information that may be of interest to you.
+      </p>
+      <p className={desc}>
+        Additionally, we may also collect data sent from your browser whenever you access our Site by or through any
+        device (“Usage Data”). Information such as your device&apos;s Internet Protocol (IP) address, browser type, the
+        pages you visited on our site, time of your visit, the amount of time spent on the Site, and other diagnostic
+        data may be included in Usage Data.
+      </p>
+
+      <p className={subTitleClass}>HOW WE COLLECT INFORMATION</p>
+      <p className={desc}>We collect information in a variety of ways, including:</p>
+      <ul className='list-disc pl-7'>
+        <li className={desc}>Directly - we collect personal information which you directly provide to us</li>
+        <li className={desc}>
+          Indirectly - we may collect personal information which you indirectly provide to us while interacting with us,
+          such as when you use our website, in emails, over the telephone and in your online enquiries
+        </li>
+        <li className={desc}>
+          From third parties - we collect personal information from third parties, such as details of your use of our
+          website from our analytics and cookie providers
+        </li>
+        <li className={desc}>
+          From publicly available sources - we collect personal data from publicly available resources such as
+          professional networking sites such as LinkedIn
+        </li>
+      </ul>
+
+      <p className={subTitleClass}>PURPOSE OF DATA PROCESSING</p>
+      <p className={desc}>CarbonX uses the collected data for various purposes such as:</p>
+      <ol className='list-decimal pl-7'>
+        <li className={desc}>To provide and maintain our Service;</li>
+        <li className={desc}>To notify Users about updates and changes to our Site;</li>
+        <li className={desc}>To provide customer support;</li>
+        <li className={desc}>
+          To contact and communicate with Users including in response to any enquiries you make with us;
+        </li>
+        <li className={desc}>To gather valuable information for further analysis to improve our Site;</li>
+        <li className={desc}>To monitor the usage and engagement of our Site;</li>
+      </ol>
+
+      <p className={subTitleClass}>SHARING & DISCLOSURE</p>
+      <p className={desc}>Under some circumstances, we may disclose information/data to:</p>
+      <ul className='list-disc pl-7'>
+        <li className={desc}>Our subsidiaries and affiliates;</li>
+        <li className={desc}>Our employees and/or related entities;</li>
+        <li className={desc}>IT service providers and data storage;</li>
+        <li className={desc}>Auditors; </li>
+        <li className={desc}>Our existing or potential agents or business partners; </li>
+        <li className={desc}>
+          Courts, tribunals, regulatory authorities and law enforcement officers, as required or authorised by law, in
+          connection with any actual or prospective legal proceedings, or in order to establish, exercise or defend our
+          legal rights; 
+        </li>
+        <li className={desc}>
+          Third parties to collect and process data, such as Google Analytics or other relevant analytics businesses; 
+        </li>
+        <li className={`list-none mt-5 ${desc}`}>
+          1. Cross Border Transfer - cross-border transfers of personal are permitted as long as the recipient nation
+          has personal data protection laws that are at least as stringent as Indonesian Personal Data Protection Law.
+          If these provisions aren’t met, the data controllers are responsible for making sure that there is sufficient,
+          legally binding protection, or at least, that the subjects of the personal data have given their consent.
+        </li>
+      </ul>
+
+      <p className={subTitleClass}>USERS RIGHTS</p>
+      <p className={`${desc} mt-3`}>
+        <strong>User&apos;s choice</strong> - Please read this Privacy Policy and Terms carefully. If you provide
+        personal information to us, you understand that we will collect, hold, make use, and disclose your provided
+        information in accordance with this Privacy Policy.
+      </p>
+      <p className={`${desc} mt-3`}>
+        <strong>Anonymity</strong> - Where feasible we will give you the option of not identifying yourself in your
+        dealings with us.
+      </p>
+      <p className={`${desc} mt-3`}>
+        <strong>Restrict and unsubscribe</strong> - to object to processing for direct marketing/unsubscribe from out
+        email database or opt-out of communications, contact us using the details below or opt-out using the opt-out
+        facilities provided in the communication.
+      </p>
+      <p className={`${desc} mt-3`}>
+        <strong>Access</strong> - you may request access to the personal information that we hold about you. An
+        administrative fee may be payable for the provision of such information. Please note, in some situations, we may
+        be legally permitted to withhold access to your personal information. If we cannot provide access to your
+        information,
+      </p>
+      <p className={`${desc} mt-3`}>
+        <strong>Correction</strong> - if any information that you provided is inaccurate, out of date, irrelevant or
+        incomplete, please contact us using the details below. We will take reasonable steps to promptly correct any
+        information found to be inaccurate, out of date, incomplete, irrelevant or misleading.
+      </p>
+      <p className={`${desc} mt-3`}>
+        <strong>Complaints</strong> - if you wish to make a complaint, please contact us using the details below and
+        provide us with full details of the complaint. We will promptly investigate your complaint and respond to you,
+        in writing, setting out the outcome of our investigation and the steps we will take in response to your
+        complaint.
+      </p>
+      <p className={desc}>Withdrawal - you may request to withdraw consent to the terms of the Agreement at anytime.</p>
+
+      <p className={subTitleClass}>DATA BREACH NOTIFICATIONS</p>
+      <p className={desc}>
+        In the event of personal data breach, CarbonX will notify the User and the MoCI within three days and will also
+        issue a public notice in the case of an interference with public services or a severe impact on public interest.
+      </p>
+      <p className={subTitleClass}>DATA RETENTION</p>
+      <p className={desc}>
+        Your personal data is retained as long as the purpose for which it was collected remains and until it is no
+        longer necessary for any other legal or business purposes. After the retention period, your personal data will
+        be deleted.
+      </p>
+      <p className={subTitleClass}>COOKIES AND SIMILAR TECHNOLOGIES</p>
+      <p className={desc}>
+        CarbonX uses cookies as long as the User uses the CarbonX site in improving the quality of the Site, to
+        recognize and remember certain information on the User&apos;s browser including session management, service
+        personalization and user activity recording. The User can choose to disable or personalize the cookies settings
+        in the User&apos;s browser, but if so, certain functions of the Site&apos;s services will be limited.
+      </p>
+      <p className={`mt-5 ${desc}`}>Examples of Cookies we use:</p>
+      <ol className='list-decimal pl-7'>
+        <li className={desc}>
+          Functional Cookies: Functional cookies are used to enhance the performance of a website as without them
+          certain functions may not be available.
+        </li>
+        <li className={desc}>
+          Performance Cookies: Performance cookies monitor site performance and follow user actions but do not collect
+          identifiable information - they collect data anonymously and use it to improve the website.
+        </li>
+      </ol>
+      <p className={desc}>
+        Preference Cookies: Preference Cookies is used to remember your preferred and various settings
+      </p>
+
+      <p className={subTitleClass}>UPDATES & CHANGES</p>
+      <p className={desc}>
+        We may update our Privacy Policy from time to time to ensure that it is consistent with any developments to the
+        way CarbonX uses your personal data or any changes to the laws and regulations applicable to CarbonX. We will
+        make available the updated Privacy Policy on our Site. All communications, transactions and dealings with us
+        shall be subject to the latest version of this policy in force at the time.
+      </p>
+      <p className={`mt-5 ${desc}`}>
+        We will let you know via email and/or a prominent notice on our Service, prior to the change becoming effective
+        and update the “effective date” at the top of this Privacy Policy.
+      </p>
+      <p className={`mt-5 ${desc}`}>
+        You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are
+        effective when they are posted on this page.
+      </p>
+
+      <p className={subTitleClass}>CONTACT US</p>
+      <p className={desc}>
+        To contact us on any aspect of this Privacy Policy or your personal data or to provide any feedback that you may
+        have, please contact us by email at admin@carbonxco.com.
+      </p>
     </SectionWrapper>
   );
 }
