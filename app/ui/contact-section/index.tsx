aa3e17015fcdeac14b0f37adb403bdb073bf76f0
arian <arian@arians-MacBook-Air.local>
2024-08-20 17:14:29 +0700

    Feat: add rounded class on home contact section

--- app/ui/contact-section/index.tsx
+++ app/ui/contact-section/index.tsx
@@ -8,8 +8,14 @@ export const ContactSection = () => {
   return (
     <>
       <SectionWrapper className='grid grid-cols-1 md:grid-cols-[auto_auto] py-20 gap-x-20' padding='high'>
-        <div className='flex justify-center items-center w-[300px] xl:w-[500px]'>
-          <Image src='/assets/home/desa-nekan.png' className='object-fit' width={500} height={500} alt='contact' />
+        <div className='flex justify-center items-center w-[300px] xl:w-[500px] rounded-tl-[30px] rounded-br-[30px]'>
+          <Image
+            src='/assets/home/desa-nekan.png'
+            className='object-fit rounded-tl-[30px] rounded-br-[30px]'
+            width={500}
+            height={400}
+            alt='contact'
+          />
         </div>
         <div className='flex flex-col justify-center'>
           <h2 className='text-[32px] font-medium mt-5 lg:mt-0'>Jump start your projects with us!</h2>
